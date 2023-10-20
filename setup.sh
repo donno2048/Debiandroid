94753df388358f638d32a54ab3201e5f1c97a0b4
Elisha Hollander <just4now666666@gmail.com>
2023-10-20 13:14:54 +0300

    use `echo -e` and path variables

--- setup.sh
+++ setup.sh
@@ -6,22 +6,15 @@ case $(uname -m) in
         i686) ARCH=i386;;
         x86_64) ARCH=amd64;;
 esac
+packages_path="var/cache/apt/archives"
 for pkg in $(wget -qO- https://raw.githubusercontent.com/donno2048/Debiandroid/master/packages.txt | sed "s/ARCH/$ARCH/g"); do
-    wget -nv -nd https://ftp.debian.org/debian/$pkg -P $PWD/var/cache/apt/archives
-done
-for pkg in $PWD/var/cache/apt/archives/*.deb; do
-    dpkg-deb --fsys-tarfile $pkg | proot --link2symlink tar -xf -
-    ar x $pkg $(ar t $pkg | grep data)
+    wget -nv -nd https://ftp.debian.org/debian/$pkg -P $packages_path
+    path="$packages_path/$(grep -o '[^/]*\.deb' <<< $pkg)"
+    ar x $path $(ar t $path | grep data)
     proot --link2symlink tar faox data.*
     rm -f data.*
 done
-echo "nameserver 8.8.8.8
-nameserver 8.8.4.4" > $PWD/etc/resolv.conf
-echo "root:x:0:0:root:/:/bin/sh
-mail:x:8:8:mail:/var/mail:/usr/sbin/nologin
-_apt:x:100:65534::/nonexistent:/usr/sbin/nologin" > $PWD/etc/passwd
-echo "Package: dpkg
-Version: $(dpkg-deb -f $(ls $PWD/var/cache/apt/archives/dpkg*.deb -1) Version)
-Maintainer: unknown
-Status: install ok installed" > $PWD/var/lib/dpkg/status
-echo "deb http://deb.debian.org/debian bullseye main" > $PWD/etc/apt/sources.list
+echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4" > etc/resolv.conf
+echo -e "root:x:0:0:root:/:/bin/sh\n_apt:x:100:65534::/nonexistent:/usr/sbin/nologin" > etc/passwd
+echo -e "Package: dpkg\nVersion: $(dpkg-deb -f $(ls $packages_path/dpkg*.deb) Version)\nMaintainer: unknown\nStatus: install ok installed" > var/lib/dpkg/status
+echo "deb http://deb.debian.org/debian bullseye main" > etc/apt/sources.list
