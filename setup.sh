f81b534b090b67a17203548402ac8d44bfb475ed
Elisha Hollander <just4now666666@gmail.com>
2023-10-25 21:48:17 +0300

    delete after setup

--- setup.sh
+++ setup.sh
@@ -21,3 +21,4 @@ echo -e "Package: dpkg\nVersion: $(dpkg-deb -f $(ls $packages_path/dpkg*.deb) Ve
 echo -e "alias debian='env -u TMPDIR -u LD_PRELOAD -u PATH proot --link2symlink -S . -w /root /bin/bash'\nexport PATH\nexport GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'\nexport PS1='\[\033[01;32m\]\u\[\033[00m\]@\[\033[01;34m\]\w\[\033[00m\]$ '\nalias ls='ls --color=auto --group-directories-first'\nalias grep='grep --color=auto'\nalias dir='dir --color=auto'\nalias diff='diff --color=auto'" >> ~/.bashrc
 echo "deb http://deb.debian.org/debian bullseye main" > etc/apt/sources.list
 echo "staff:!:1:" >> etc/group
+rm $0
