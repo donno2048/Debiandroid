ccff0a14820273428706a7b43ffd375e374f0266
Elisha Hollander <just4now666666@gmail.com>
2025-08-11 13:37:41 +0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-apt="env -u LD_PRELOAD -u TMPDIR proot -l -S . /usr/bin/apt"
+apt="env -u LD_PRELOAD -u TMPDIR proot -l -0 -r . /usr/bin/apt"
 case $(uname -m) in
         arm) ARCH=armel;;
         aarch64) ARCH=arm64;;
@@ -20,7 +20,7 @@ ln -sf /usr/share/zoneinfo/UTC etc/localtime
 echo -e "nameserver 8.8.8.8\nnameserver 8.8.4.4" > etc/resolv.conf
 echo -e "root:x:0:0:root:/:/bin/sh\n_apt:x:100:65534::/nonexistent:/usr/sbin/nologin\nuucp:x:10:10:uucp:/var/spool/uucp:/bin/sh\nmail:x:8:8:mail:/var/mail:/bin/sh\nman:x:6:12:man:/var/cache/man:/usr/sbin/nologin" > etc/passwd
 echo -e "Package: dpkg\nVersion: $(dpkg-deb -f $(ls $packages_path/dpkg*.deb) Version)\nMaintainer: unknown\nStatus: install ok installed" > var/lib/dpkg/status
-echo -e "alias debian='env -u PREFIX -u TMPDIR -u LD_PRELOAD proot -l -S . -w /root /bin/bash'\nexport TMPDIR='/tmp'\nexport PATH='/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:$PATH'\nexport GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'\nexport PS1='\[\033[01;32m\]\u\[\033[00m\]@\[\033[01;34m\]\w\[\033[00m\]$ '\nalias ls='ls --color=auto --group-directories-first'\nalias grep='grep --color=auto'\nalias dir='dir --color=auto'\nalias diff='diff --color=auto'\nexport LC_ALL=C\nexport DEBIAN_FRONTEND=readline\nalias sudo=" >> ~/.bashrc
+echo -e "alias debian='env -u PREFIX -u TMPDIR -u LD_PRELOAD proot -l -0 -r . -w /root /bin/bash'\nexport TMPDIR='/tmp'\nexport PATH='/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:$PATH'\nexport GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'\nexport PS1='\[\033[01;32m\]\u\[\033[00m\]@\[\033[01;34m\]\w\[\033[00m\]$ '\nalias ls='ls --color=auto --group-directories-first'\nalias grep='grep --color=auto'\nalias dir='dir --color=auto'\nalias diff='diff --color=auto'\nexport LC_ALL=C\nexport DEBIAN_FRONTEND=readline\nalias sudo=" >> ~/.bashrc
 echo -e "root:x:0:\nstaff:x:50:\nuucp:x:10:\nmail:x:8:mail\nadm:x:4:\nutmp:x:43:" >> etc/group
 echo "deb http://deb.debian.org/debian bullseye main" > etc/apt/sources.list
 $apt update --allow-insecure-repositories
